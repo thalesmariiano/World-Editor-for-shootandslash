0e6cb465da994c870019a0533da21649447d068d
= <thales.mariano125@gmail.com>
2025-02-26 13:49:39 -0300

    feat: desconstruct mapEditor function && add engine

--- public/arquivos/js/mapEditor.js
+++ public/arquivos/js/mapEditor.js
@@ -1,214 +1,187 @@
 
-function mapEditor(canvasId, image){
-	const canvas = document.querySelector(canvasId)
-	const ctx = canvas.getContext('2d')
-	canvas.imageSmoothingEnabled = false
+const engine = new Engine('#editor-display')
 
-	const editable_area = {
+const map_editor = {
+	area: {
 		width: 2000,
 		height: 800
-	}
-	const pointer = {
+	},
+	pointer: {
 		x: 0,
 		y: 0, 
 		color: 'white', 
 		size: 50
-	}
-
-	const tilemap = new Image()
-	tilemap.src = image
-
-	const tiles = []
-
-	let moveMapEnabled = false
-	let moveMouseTrigger = false
-
-	let click_mode = 'addTile'
-
-	let transX = 0
-	let transY = 0
-	let startX = 0
-	let startY = 0
-
-	const getPointerX = e => roundTo(e.clientX - canvas.offsetLeft - pointer.size/2 - transX, pointer.size)
-	const getPointerY = e => roundTo(e.clientY - canvas.offsetTop - pointer.size/2 - transY, pointer.size)
-	const clearCanvas = () => {if(tiles.length) tiles.length = 0}
-
-	function moveMap(){
-		moveMapEnabled = !moveMapEnabled
-
-		if(!moveMapEnabled) switchMode('addTile')
-		else switchMode('')
-	}
-
-	function movePointer(e){
-		pointer.x = getPointerX(e)
-		pointer.y = getPointerY(e)
-
-		if(moveMapEnabled) canvas.classList.add('cursor-move')
-		else canvas.classList.remove('cursor-move')
-		
-		if(moveMapEnabled && moveMouseTrigger){
-			transX = e.clientX - startX
-			transY = e.clientY - startY
-		}
-
-		// continuous tile placement
-		if(parseInt(localStorage.getItem('tilePlacement'))){
-			if(click_mode == 'addTile' && moveMouseTrigger){
-				addTile(e)
-			}
-		}
-		
-	}
-
-	function clickHandler(e){
-		const handlers = {
-			'addTile': addTile,
-			'removeTile': removeTile,
-			'': () => {}
-		}
-		handlers[click_mode](e)
-	}
-
-	function mouseDownHandler(e){
-		moveMouseTrigger = true
-		startX = e.clientX - transX
-		startY = e.clientY - transY
-	}
-
-	function mouseUpHandler(e){
-		moveMouseTrigger = false
-	}
-
-	function addTile(e){
-		const tileX = getPointerX(e)
-		const tileY = getPointerY(e)
-
-		// if is out editable area, return
-		if(tileX >= editable_area.width ||
-		   tileX < 0 ||
-		   tileY >= editable_area.height ||
-		   tileY < 0
-		) return
-
-
-		// replace a existing tile for another or not
-		const tileIndex = tiles.findIndex(tile => tileX == tile.position.x && tileY == tile.position.y)
-		if(parseInt(localStorage.getItem('replaceTile')) && tileIndex !== -1){
-			if(tileIndex !== -1) tiles.splice(tileIndex, 1)	
-		}else if(tileIndex !== -1) return
-
-		
-		// push tile object on array
-		tiles.push({
-			width: 50,
-			height: 50,
-			imgX: config.tileImgX,
-			imgY: config.tileImgY,
-			position: {x: tileX, y: tileY},
-			type: 'Block',
-			visible: false
-		})
-	}
-
-	function removeTile(e){
-		const tileX = getPointerX(e)
-		const tileY = getPointerY(e)
-
-		const tileIndex = tiles.findIndex(tile => tileX == tile.position.x && tileY == tile.position.y)
-		if(tileIndex !== -1) tiles.splice(tileIndex, 1)					
-	}
-
-	function switchMode(mode){
-		click_mode = mode
-		
-		if(click_mode == 'addTile') pointer.color = 'white'
-		else if(click_mode == 'removeTile') pointer.color = 'red'
-	}
-
-	function switchTexture(x,y){
-		moveMapEnabled = false
-		config.tileImgX = x
-		config.tileImgY = y
-		switchMode('addTile')
-	}
-
-	function render(){
-		ctx.clearRect(0, 0, canvas.width, canvas.height)
-		ctx.save()
-
-		canvas.width = innerWidth
-		canvas.height = innerHeight
-
-		ctx.translate(transX, transY)
-
-		// Editable area canvas background
-		ctx.fillStyle = '#606060'
-		ctx.fillRect(0,0, editable_area.width, editable_area.height)
-		//
-		ctx.strokeStyle = 'white'
-		ctx.strokeRect(0, 0, editable_area.width, editable_area.height)
-		//
-
-		ctx.font = '20px Arial'
-		ctx.fillStyle = '#303030'
-		ctx.fillText("Player spawn", 95, 360)
-
-		ctx.fillStyle = '#404040'
-		ctx.fillRect(127, 380, 55, 100)
-
-		tiles.forEach(tile => {
-			ctx.drawImage(
-				tilemap,
-				tile.imgX,
-				tile.imgY,
-				32, 32,
-				tile.position.x,
-				tile.position.y,
-				tile.width, 
-				tile.height
-			)		
-		})
-
-		// pointer
-		ctx.strokeStyle = pointer.color
-		ctx.strokeRect(pointer.x, pointer.y, pointer.size, pointer.size)
-		// 
-
-		if(click_mode == 'addTile'){
-			ctx.globalAlpha = 0.5
-			ctx.drawImage(
-				tilemap,
-				config.tileImgX,
-				config.tileImgY,
-				32, 32,
-				pointer.x,
-				pointer.y,
-				pointer.size, 
-				pointer.size
-			)	
-		}
-		
-		ctx.restore()
-		requestAnimationFrame(render)
-	}
-
-	function init(){
-		canvas.addEventListener('click', clickHandler)
-		canvas.addEventListener('mousemove', movePointer)
-		canvas.addEventListener('mousedown', mouseDownHandler)
-		canvas.addEventListener('mouseup', mouseUpHandler)
-
-		render()
-	}
-
-	return {
-		switchTexture,
-		clearCanvas,
-		switchMode,
-		moveMap,
-		tiles,
-		init
-	}
+	},
+	tiles: [],
+	transX: 0,
+	transY: 0,
+	startX: 0,
+	startY: 0,
+	clickMode: 'addTile',
+	moveMapEnabled: false,
+	moveMouseTrigger: false,
 }
+
+engine.on('render', () => {
+	engine.BUFFER.fillStyle = "#404040"
+	engine.BUFFER.fillRect(0, 0, innerWidth, innerHeight)
+
+	engine.BUFFER.translate(map_editor.transX, map_editor.transY)
+
+	// Editable area canvas background
+	engine.BUFFER.fillStyle = '#606060'
+	engine.BUFFER.fillRect(0,0, map_editor.area.width, map_editor.area.height)
+	//
+	engine.BUFFER.strokeStyle = 'white'
+	engine.BUFFER.strokeRect(0, 0, map_editor.area.width, map_editor.area.height)
+	//
+
+	engine.BUFFER.font = '20px Arial'
+	engine.BUFFER.fillStyle = '#303030'
+	engine.BUFFER.fillText("Player spawn", 95, 360)
+
+	engine.BUFFER.fillStyle = '#404040'
+	engine.BUFFER.fillRect(127, 380, 55, 100)
+
+	map_editor.tiles.forEach(tile => {
+		engine.BUFFER.drawImage(
+			tilemap,
+			tile.imgX,
+			tile.imgY,
+			32, 32,
+			tile.position.x,
+			tile.position.y,
+			tile.width, 
+			tile.height
+		)		
+	})
+
+	// pointer
+	engine.BUFFER.strokeStyle = map_editor.pointer.color
+	engine.BUFFER.strokeRect(
+		map_editor.pointer.x,
+		map_editor.pointer.y, 
+		map_editor.pointer.size, 
+		map_editor.pointer.size
+	)
+	// 
+
+	if(map_editor.clickMode == 'addTile'){
+		engine.BUFFER.globalAlpha = 0.5
+		engine.BUFFER.drawImage(
+			tilemap,
+			config.tileImgX,
+			config.tileImgY,
+			32, 32,
+			map_editor.pointer.x,
+			map_editor.pointer.y,
+			map_editor.pointer.size, 
+			map_editor.pointer.size
+		)	
+	}
+})
+
+engine.canvas.addEventListener('click', e => {
+	const handlers = {
+		'addTile': addTile,
+		'removeTile': removeTile,
+		'': () => {}
+	}
+	handlers[map_editor.clickMode](e)
+})
+
+engine.canvas.addEventListener('mousemove', e => {
+	movePointer(e)
+})
+
+engine.canvas.addEventListener('mousedown', e => {
+	map_editor.moveMouseTrigger = true
+	map_editor.startX = e.clientX - map_editor.transX
+	map_editor.startY = e.clientY - map_editor.transY
+})
+
+engine.canvas.addEventListener('mouseup', e => {
+	map_editor.moveMouseTrigger = false
+})
+
+engine.resizeAspectRatio()
+engine.start()
+
+window.onresize = () => engine.resizeAspectRatio()
+
+const switchMode = mode => {
+	map_editor.clickMode = mode
+	
+	if(map_editor.clickMode == 'addTile') map_editor.pointer.color = 'white'
+	else if(map_editor.clickMode == 'removeTile') map_editor.pointer.color = 'red'
+}
+
+const switchTexture = (x,y) => {
+	map_editor.moveMapEnabled = false
+	config.tileImgX = x
+	config.tileImgY = y
+	switchMode('addTile')
+}
+
+const addTile = e => {
+	const tileX = getPointerX(e)
+	const tileY = getPointerY(e)
+
+	// if is out editable area, return
+	if(tileX >= map_editor.area.width || tileX < 0 || tileY >= map_editor.area.height || tileY < 0) return
+
+	// replace a existing tile for another or not
+	const tileIndex = map_editor.tiles.findIndex(tile => tileX == tile.position.x && tileY == tile.position.y)
+	if(parseInt(localStorage.getItem('replaceTile')) && tileIndex !== -1){
+		if(tileIndex !== -1) tiles.splice(tileIndex, 1)	
+	}else if(tileIndex !== -1) return
+	
+	// push tile object on array
+	map_editor.tiles.push({
+		width: 50,
+		height: 50,
+		imgX: config.tileImgX,
+		imgY: config.tileImgY,
+		position: {x: tileX, y: tileY},
+		type: 'Block',
+		visible: false
+	})
+}
+
+const removeTile = e => {
+	const tileX = getPointerX(e)
+	const tileY = getPointerY(e)
+
+	const tileIndex = map_editor.tiles.findIndex(tile => tileX == tile.position.x && tileY == tile.position.y)
+	if(tileIndex !== -1) map_editor.tiles.splice(tileIndex, 1)					
+}
+
+const movePointer = e => {
+	map_editor.pointer.x = getPointerX(e)
+	map_editor.pointer.y = getPointerY(e)
+
+	if(map_editor.moveMapEnabled) engine.canvas.classList.add('cursor-move')
+	else engine.canvas.classList.remove('cursor-move')
+	
+	if(map_editor.moveMapEnabled && map_editor.moveMouseTrigger){
+		map_editor.transX = e.clientX - map_editor.startX
+		map_editor.transY = e.clientY - map_editor.startY
+	}
+
+	// continuous tile placement
+	if(parseInt(localStorage.getItem('tilePlacement'))){
+		if(map_editor.clickMode == 'addTile' && map_editor.moveMouseTrigger){
+			addTile(e)
+		}
+	}
+	
+}
+
+const moveMap = () => {
+	map_editor.moveMapEnabled = !map_editor.moveMapEnabled
+
+	if(!map_editor.moveMapEnabled) switchMode('addTile')
+	else switchMode('')
+}
+
+const clearCanvas = () => map_editor.tiles.length = 0
