a59812eef0601877304c3f7dfed170f240542d87
= <thales.mariano125@gmail.com>
2025-02-26 17:05:40 -0300

    feat: add pointer class

---
+++ public/arquivos/js/Classes/Pointer.js
@@ -0,0 +1,68 @@
+
+class Pointer {
+    constructor(element){
+        this.element = element
+
+        this.element.addEventListener('click', e => {
+            const listeners = this.eventsCallback.filter(event => event.name === 'click')
+            listeners.forEach(listener => listener.callback(e))
+        })
+        
+        this.element.addEventListener('mousemove', e => {
+            const listeners = this.eventsCallback.filter(event => event.name === 'move')
+            listeners.forEach(listener => listener.callback(e))
+        })
+        
+        this.element.addEventListener('mousedown', e => {
+            const listeners = this.eventsCallback.filter(event => event.name === 'mousedown')
+            listeners.forEach(listener => listener.callback(e))
+        })
+        
+        this.element.addEventListener('mouseup', e => {
+            const listeners = this.eventsCallback.filter(event => event.name === 'mouseup')
+            listeners.forEach(listener => listener.callback(e))
+        })
+
+        this.position = {
+            x: 0,
+            y: 0
+        }
+        this.color = 'white',
+        this.size = {
+            width: 50,
+            height: 50
+        },
+
+        this.eventsCallback = []
+    }
+
+    on(event, callback){
+        this.eventsCallback.push({
+            name: event,
+            callback
+        })
+    }
+    
+    getPointerX(e){
+        return roundTo(
+            e.clientX - this.element.offsetLeft - this.size.width/2 - map_editor.transX,
+            this.size.width
+        )
+    }
+    getPointerY(e){
+        return roundTo(
+            e.clientY - this.element.offsetTop - this.size.height/2 - map_editor.transY,
+            this.size.width
+        )
+    }
+
+    draw(BUFFER){
+        BUFFER.strokeStyle = this.color
+	    BUFFER.strokeRect(
+		    this.position.x,
+		    this.position.y, 
+		    this.size.width, 
+		    this.size.height
+	    )
+    }
+}
